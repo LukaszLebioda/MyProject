67832f2eac5a1beb0be55d18259709ddec5cec6c
Łukasz Lebioda <llebioda@novomatic-tech.com>
2025-06-19 16:28:46 +0200

    changes forbid only in playwright config

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -5,12 +5,10 @@ export default defineConfig({
 	timeout: 15 * 1000, // locator timeout, default -> 30000
 	expect: { timeout: 5000 }, // assertion timeout, default -> 5000
 	fullyParallel: false, // false -> tests will run sequentially within single spec file; true -> tests will run parallelly within single spec file, and each test will be executed in independent worker;
-	forbidOnly: false, // default -> !!process.env.CI
+	forbidOnly: false, // default -> !!process.env.CI // test.only will fail in CI environment
 	retries: 1, // default -> process.env.CI ? 2 : 1
 	workers: process.env.CI ? 1 : 1, // default -> ? 1 : undefined; worker is a new instance of web browser (in incognito mode); by default playwright creates 1 separate worker for each spec file (by default max 5);
-	reporter: [
-		["html", { open: "never", outputFolder: "./Playwright/playwright-report" }],
-	], // or open: "on-failure" (other reporter -> line); outputFolder is optional, by default reports are stored in main directory
+	reporter: [["html", { open: "never", outputFolder: "./Playwright/playwright-report" }]], // or open: "on-failure" (other reporter -> line); outputFolder is optional, by default reports are stored in main directory
 	outputDir: "./Playwright/test-results/", // directory to store test results; it's optional, by default reports are stored in main directory
 	use: {
 		// baseURL: 'http://127.0.0.1:3000',
